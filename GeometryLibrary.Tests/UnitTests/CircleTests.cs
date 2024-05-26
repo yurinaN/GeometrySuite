e88dc1606ce6db46840feaf3dbd83ca80ddf5ddf
yurina <yurina@apri-code.ru>
2024-05-26 19:44:12 +0400

    Добавлены тесты для класса Circle и Triangle

---
+++ GeometryLibrary.Tests/UnitTests/CircleTests.cs
@@ -0,0 +1,15 @@
+﻿using GeometryLibrary.Models;
+
+namespace GeometryLibrary.Tests.UnitTests
+{
+    public class CircleTests
+    {
+        [Fact]
+        public void CalculateArea_ReturnsCorrectArea()
+        {
+            var circle = new Circle(5);
+            var area = circle.CalculateArea();
+            Assert.Equal(Math.PI * 25, area, 5);
+        }
+    }
+}
---
+++ GeometryLibrary.Tests/UnitTests/TriangleTests.cs
@@ -0,0 +1,31 @@
+﻿using GeometryLibrary.Models;
+
+namespace GeometryLibrary.Tests.UnitTests
+{
+    public class TriangleTests
+    {
+        [Fact]
+        public void CalculateArea_ReturnsCorrectArea()
+        {
+            var triangle = new Triangle(3, 4, 5);
+            var area = triangle.CalculateArea();
+            Assert.Equal(6, area, 5);
+        }
+
+        [Fact]
+        public void IsRightTriangle_ReturnsTrue_ForRightTriangle()
+        {
+            var triangle = new Triangle(3, 4, 5);
+            var isRight = triangle.IsRightTriangle();
+            Assert.True(isRight);
+        }
+
+        [Fact]
+        public void IsRightTriangle_ReturnsFalse_ForNonRightTriangle()
+        {
+            var triangle = new Triangle(3, 4, 6);
+            var isRight = triangle.IsRightTriangle();
+            Assert.False(isRight);
+        }
+    }
+}
